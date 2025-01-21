c504e54745f43de64df47f17c9dc485e1c52b0da
Alberto <al2509002@gmail.com>
2025-01-21 19:32:16 -0300

    update8

---
+++ learn_cobol21.cbl
@@ -0,0 +1,30 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "APRENDIENDO COBOL21".
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  IN-AREA.
+           05 IN-DATA1 PIC X.
+           05 IN-DATA2 PIC XX.
+
+       01  R-NUM PIC 9.
+
+       01  SAVE-AREA.
+           05 SAVE-REC1 PIC X.
+           05 SAVE-REC2 PIC XX.
+
+       01  S-NUM PIC 9.
+
+       PROCEDURE DIVISION.
+       INICIO.
+           PERFORM COPY-PROCESS.
+           PERFORM COPY-PROCESS.
+       COPY-PROCESS.
+           MOVE IN-DATA1 TO SAVE-REC1.
+           MOVE IN-DATA2 TO SAVE-REC2.
+           MOVE R-NUM TO S-NUM.
+
+       FIN.
+           STOP RUN.
