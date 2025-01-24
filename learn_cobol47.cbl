0711900c78c93bc586154db69af50994b4d24cea
Alberto <al2509002@gmail.com>
2025-01-24 11:05:21 -0300

    update18.5

---
+++ learn_cobol47.cbl
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "APRENDIENDO COBOL47".
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  SI-O-NO PIC X.
+
+       PROCEDURE DIVISION.
+       INICIO.
+           PERFORM CONTINUAR.
+           IF SI-O-NO = "N"
+               GO TO FIN.
+
+           PERFORM LOGICA-PRINCIPAL.
+       FIN.
+           STOP RUN.
+
+       CONTINUAR.
+           DISPLAY "CONTINUAR (Y/N)?".
+           ACCEPT SI-O-NO.
+           IF SI-O-NO = "n"
+               MOVE ""
