200ca34401d174983bf46b0ff32398f0d4b09ca9
Alberto <al2509002@gmail.com>
2025-01-27 23:56:39 -0300

    update21

--- learn_cobol50.cbl
+++ learn_cobol50.cbl
@@ -1,32 +1,40 @@
-      ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
-      * Tectonics: cobc
-      ******************************************************************
        IDENTIFICATION DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
-       ENVIRONMENT DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       CONFIGURATION SECTION.
-      *-----------------------
-       INPUT-OUTPUT SECTION.
-      *-----------------------
-       DATA DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       FILE SECTION.
-      *-----------------------
-       WORKING-STORAGE SECTION.
-      *-----------------------
-       PROCEDURE DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       MAIN-PROCEDURE.
-      **
-      * The main procedure of the program
-      **
-            DISPLAY "Hello world"
-            STOP RUN.
-      ** add other procedures here
-       END PROGRAM YOUR-PROGRAM-NAME.
+       PROGRAM-ID. "APRENDIENDO COBOL50".
 
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  DIA PIC 99.
+           88 LUNES VALUE 1.
+           88 MARTES VALUE 2.
+           88 MIERCOLES VALUE 3.
+           88 JUEVES VALUE 4.
+           88 VIERNES VALUE 5.
+           88 SABADO VALUE 6.
+           88 DOMINGO VALUE 7.
+
+       PROCEDURE DIVISION.
+       INICIO.
+           DISPLAY "INGRESE UNA OPCIÓN (1-9):"
+           ACCEPT DIA.
+           EVALUATE DIA
+               WHEN 1
+                   DISPLAY "ELEGISTE EL LUNES!"
+               WHEN  2
+                   DISPLAY "ELEGISTE EL MARTES"
+               WHEN  3
+                   DISPLAY "ELEGISTE EL MIERCOLES"
+               WHEN 4
+                   DISPLAY "ELEGISTE EL JUEVES"
+               WHEN  5
+                   DISPLAY "ELEGISTE EL VIERNES"
+               WHEN  6
+                   DISPLAY "ELEGISTE EL SABADO"
+               WHEN 7
+                   DISPLAY "ELEGISTE EL DOMINGO"
+               WHEN OTHER
+                   DISPLAY "OPCIÓN FUERA DE RANGO"
+           END-EVALUATE.
+           STOP RUN.
+       FIN.
